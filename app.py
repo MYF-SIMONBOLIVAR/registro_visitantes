081c7433a066e470c4f98ad40933ee24889f9591
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2025-06-19 14:53:51 -0500

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pytz import timezone
 import json
+from streamlit_drawable_canvas import st_canvas
 
 
 # Zona horaria de Colombia
