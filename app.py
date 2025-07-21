144b7ac5eea8527a0d69cb6dc36b998eb54426b2
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2025-07-21 14:40:23 -0500

    app.py

--- app.py
+++ app.py
@@ -23,10 +23,9 @@ SHEET_ID = "1bhqn8AC_MbZhLPt44rs5OQ4IVLYG4-oOwNK_uAmw1hM"
 SHEET_NAME = "Hoja"
 sheet = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
 
-
 col1,col2 = st.columns([1, 4])
 with col1:
-st.image("logo.png", width=150) 
+    st.image("logo.png", width=150) 
 with col2:
     st.markdown(
         "<h1 style='margin-bottom: 0; color: #19287f; text-align: center; font-size: 2rem;'>"
